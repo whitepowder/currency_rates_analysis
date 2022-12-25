f47e79f7f5c2522ddbd59680050b70ceb9d302ad
whitepowder <artwhitelake@gmail.com>
2022-12-26 01:29:26 +0600

    add context manager

--- dags/utils/features_cal.py
+++ dags/utils/features_cal.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from dataclasses import dataclass
 from utils.params import db_engine, db_schema, features_query
 
-conn = create_engine(db_engine)
+engine = create_engine(db_engine)
 
 
+@dataclass
 class Feature:
-    def __init__(self, cur_list=None):
-        self._cur_list = cur_list
 
     @staticmethod
-    def feature_calc(cur_list):
-        for cur in cur_list:
-            df = pd.read_sql(features_query + f"""'{cur}'""", con=conn)
-            df['moving_average'] = df['rate'].rolling(window=2, center=True).mean()
-            df['expanding_average'] = df['rate'].expanding().mean()
-            db_name = f"{cur}_features"
-            df.to_sql(db_name.lower(), con=conn, schema=db_schema, if_exists="replace", index=False)
+    def feature_calc(currency_data):
+        for currency in currency_data:
+            with engine.connect() as conn:
+                df = pd.read_sql(features_query + f"""'{currency}'""", con=conn)
+                df['moving_average'] = df['rate'].rolling(window=2, center=True).mean()
+                df['expanding_average'] = df['rate'].expanding().mean()
+                db_name = f"{currency}_features"
+                df.to_sql(db_name.lower(), con=conn, schema=db_schema, if_exists="replace", index=False)
+                df.to_sql(db_name.lower(), con=conn, schema=db_schema, if_exists="replace", index=False)
